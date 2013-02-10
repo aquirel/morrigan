ec853e7df284c1aa32970b968eefd45f15017e81
Aquirel <Aquirel@6811b303-ec13-b609-74e4-42ffe1855865>
2013-02-10 01:39:19 +0000

    Improving viewer.

--- viewer_main.c
+++ viewer_main.c
@@ -235,6 +235,7 @@ bool process_events(bool *need_redraw)
 void draw(const Landscape *l)
 {
     glClearColor(0, 0, 0, 0);
+    glClearDepth(0.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glPushMatrix();

c27e292dd1e577b1e9547eb02ffc08c595edfb9e
Aquirel <Aquirel@6811b303-ec13-b609-74e4-42ffe1855865>
2013-02-27 02:10:02 +0000

    Debug improvements.

--- debug.h
+++ debug.h
@@ -15,11 +15,11 @@
 #endif
 
 #define clean_errno() (0 == errno ? "None" : strerror(errno))
-#define log_err(MESSAGE, ...) fprintf(stderr, "[ERROR] (%s at %s:%d: errno: %s): " MESSAGE "\n", __func__, __FILE__, __LINE__, clean_errno(), __VA_ARGS__)
-#define log_warn(MESSAGE, ...) fprintf(stderr, "[WARN] (%s at %s:%d: errno: %s): " MESSAGE "\n", __func__, __FILE__, __LINE__, clean_errno(), __VA_ARGS__)
+#define log_error(MESSAGE, ...) fprintf(stderr, "[ERROR] (%s at %s:%d: errno: %s): " MESSAGE "\n", __func__, __FILE__, __LINE__, clean_errno(), __VA_ARGS__)
+#define log_warning(MESSAGE, ...) fprintf(stderr, "[WARN] (%s at %s:%d: errno: %s): " MESSAGE "\n", __func__, __FILE__, __LINE__, clean_errno(), __VA_ARGS__)
 #define log_info(MESSAGE, ...) fprintf(stderr, "[INFO] (%s at %s:%d): " MESSAGE "\n", __func__, __FILE__, __LINE__, __VA_ARGS__)
-#define check(CONDITION, MESSAGE, ...) if(!(CONDITION)) { log_err(MESSAGE, __VA_ARGS__); errno = 0; goto error; }
-#define sentinel(MESSAGE, ...)  { log_err(MESSAGE, __VA_ARGS__); errno = 0; goto error; }
+#define check(CONDITION, MESSAGE, ...) if(!(CONDITION)) { log_error(MESSAGE, __VA_ARGS__); errno = 0; goto error; }
+#define sentinel(MESSAGE, ...)  { log_error(MESSAGE, __VA_ARGS__); errno = 0; goto error; }
 #define check_mem(CONDITION) check((CONDITION), "Out of memory.", "")
 #define check_debug(CONDITION, MESSAGE, ...) if(!(CONDITION)) { debug(MESSAGE, __VA_ARGS__); errno = 0; goto error; }
 
